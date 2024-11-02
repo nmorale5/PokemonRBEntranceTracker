f42a2216abf1a247a9786148e3b762f2fce64aa4
Adam <halaffamagician@gmail.com>
2024-11-01 23:01:07 -0400

    endpoint specs

--- src/backend/generateGraph.ts
+++ src/backend/generateGraph.ts
@@ -1,36 +1,43 @@
-console.log("Ugh I have to remember Typescript again...")
-
-enum EntranceAccessibility {
+export enum EntranceAccessibility {
     "Inaccessible",
     "Accessible",
     "Other",
 }
 
-class Warp {
+export enum CheckAccessibility {
+    "Inaccessible",
+    "Accessible",
+    "Other",
+}
+
+export class Warp {
 
 }
 
-function entranceAccessible(entrance: Warp) : EntranceAccessibility {
-    return EntranceAccessibility.Other
+export function entranceAccessible(entrance: Warp) : EntranceAccessibility {
+    /**
+     * Parameters:
+     *  Warp: Representation of an entrance
+     * Returns enum EntranceAccessibility
+     */
+    return EntranceAccessibility.Other;
 }
-// Parameters:
-// location: String representation of an entrance
-// Returns enum EntranceAccessibility
-// Location is both accessible and has been taken = 2
-// Location is currently accessible based on current data (graph?) = 1
-// Location is not currently accessible based on current data = 0
 
-// checkAccessible(check: string)
-// Parameters:
-// check: String representation of a check
-// Returns enum CheckAccessibility
-// Check has been received = 2 (backend should not be concerned with the frontend information of whether it was received)
-// (optional) special cases for is accessible but not in logic e.g. (2+)
-// Check is currently accessible based on current data (graph?) = 1
-// Check is not currently accessible based on current data = 0
+export function checkAccessible(check: string) : CheckAccessibility {
+    /**
+     * Parameters:
+     *  check: Respresentation of a check
+     * Return enum CheckAccessibility
+     */
+    return CheckAccessibility.Other;
+}
 
-// shortestPath(startRegion: string, endRegion: string)
-// Parameters:
-// endRegion: String representation of a location
-// Returns Array<String>
-// Ordered array of string locations to enter to arrive at endLoc, starting with the first warp (NOT the starting location) and ending with the final warp (endRegion), length-1 array containing just location if at location
+export function shortestPath(startRegion: string, endRegion: string) : Array<string> {
+    /**
+     * Parameters:
+     *  startRegion: start region string
+     *  endRegion: destination region string
+     * Returns: Ordered array of string locations to enter to arrive at endLoc, not including startLoc
+     */
+    return [];
+}
