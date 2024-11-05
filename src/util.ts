b90adda1c2014af1bae469146958fa4c05abbf1a
Noah Morales <nmorale5@mit.edu>
2024-11-04 23:46:41 -0500

    create Map.tsx and tidy up a bit

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,10 @@
+import L from "leaflet";
+
 export async function readJsonFile(filename: string): Promise<any> {
-    const response = await fetch(filename);
-    return response.json();
+  const response = await fetch(filename);
+  return response.json();
+}
+
+export function latLngFromPixelCoordinates(x: number, y: number): L.LatLng {
+  return L.latLng(.7200 - y / 10000, x / 10000);
 }
