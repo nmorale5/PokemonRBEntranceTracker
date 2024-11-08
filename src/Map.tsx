a4204d423e11ce70ab8cce30e6d60ab878b281c5
Noah Morales <nmorale5@mit.edu>
2024-11-07 22:10:15 -0500

    manually added all check locations on map

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,17 +1,17 @@
 import React, { useEffect } from "react";
 import "./Map.css";
 import L from "leaflet";
-import { latLngFromPixelCoordinates } from "./util";
+import { latLngFromPixelCoordinates, pixelCoordinatesFromLatLng } from "./util";
 import warpData from "./PokemonData/warps.json";
+import checkData from "./PokemonData/CheckData.json";
 
 export default function Map() {
   useEffect(() => {
     const map = L.map("map").setView(latLngFromPixelCoordinates(3600, 3600));
     const latLngBounds = L.latLngBounds(
       latLngFromPixelCoordinates(0, 0),
-      latLngFromPixelCoordinates(7200, 7200)
+      latLngFromPixelCoordinates(7200, 7200),
     );
-
     L.imageOverlay("/PokemonRedMapNoArrows.png", latLngBounds, {
       alt: "Pokemon Red/Blue Map",
       interactive: true,
@@ -19,20 +19,27 @@ export default function Map() {
 
     map.fitBounds(latLngBounds);
 
-    interface Entrance {
-      x: number;
-      y: number;
-      region: string;
-    }
-
-    const warps = warpData as { from: Entrance; to: Entrance }[];
-    warps.forEach((warp) => {
+    warpData.forEach((warp) => {
       L.marker(latLngFromPixelCoordinates(warp.from.x, warp.from.y), {
         title: `${warp.from.region} to ${warp.to.region}`,
       }).addTo(map);
       L.marker(latLngFromPixelCoordinates(warp.to.x, warp.to.y), {
         title: `${warp.to.region} to ${warp.from.region}`,
       }).addTo(map);
+    });
+
+    checkData.forEach((check) => {
+      if (!check.coordinates) {
+        return;
+      }
+      L.marker(latLngFromPixelCoordinates(check.coordinates.x, check.coordinates.y), {
+        title: check.name,
+      }).addTo(map);
+    });
+
+    map.on("click", async function(e) {
+      const [x, y] = pixelCoordinatesFromLatLng(e.latlng).map(num => Math.round((num + 8) / 16) * 16 - 8); // get the center of the nearest 16x16 square
+      await navigator.clipboard.writeText(`,\n    "coordinates": { "x": ${x}, "y": ${y} }`);
     });
 
     return () => {
--- src/util.ts
+++ src/util.ts
@@ -8,3 +8,8 @@ export async function readJsonFile(filename: string): Promise<any> {
 export function latLngFromPixelCoordinates(x: number, y: number): L.LatLng {
   return L.latLng(0.72 - y / 10000, x / 10000);
 }
+
+export function pixelCoordinatesFromLatLng(latlng: L.LatLng): [number, number] {
+  // @ts-ignore
+  return [latlng.lng * 10000, 7200 - latlng.lat * 10000].map(Math.round);
+}
