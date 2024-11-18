f33f8dceeb8ed4869200af35a236f17a69bb77b7
Adam <halaffamagician@gmail.com>
2024-11-17 20:31:25 -0500

    Toggles for events

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Map from "./Components/Map";
 import "./App.css";
 import Login from "./Components/Login";
 import Save from "./Components/Save";
+import Items from "./Components/Items";
 
 export default function App() {
   return (
     <div className="App">
       <Login />
+      <Items />
       <Save />
       <Map />
     </div>
